7088d42f2e28d44e87ccf7c8a567f8a70d29ffe0
Prashant Bhadane <pkbhadane545@gmail.com>
2026-05-08 23:33:53 +0530

    set sameSite in 'lax' to 'none'

--- Utilities/handleJsonWebToken.js
+++ Utilities/handleJsonWebToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
 dotenv.config()
 export const jwtToken = (User) => {
-  const paylod = { userId: User._id };
+  const paylod = { userId: User?._id };
   const secretKey = process.env.JWT_SECRETKEY;
   const expiresIn = process.env.JWT_EXPIRES;
 
